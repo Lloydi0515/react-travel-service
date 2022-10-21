68597a7e16c53f48724e77f5f761b0ffc7866da5
Lloyd Fernandez <lloydfernandez15@gmail.com>
2022-10-21 12:10:34 +0800

    feature add about us file

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 import AboutImg from "../assets/night.jpg";
 import "../components/HeroStyles.css";
 import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 function About() {
   return (
     <>
       <Navbar />
       <Hero cName="hero-mid" heroImg={AboutImg} title="About" btnClass="hide" />
+      <AboutUs />
       <Footer />
     </>
   );
